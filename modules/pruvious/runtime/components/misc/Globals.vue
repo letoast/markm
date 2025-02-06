435c33c56e3c7d09d0f9ee311f96c7ee911bda40
letoast <ciroman@gmail.com>
2025-02-06 23:30:20 +0100

    wip

---
+++ modules/pruvious/runtime/components/misc/Globals.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <PruviousLoadingIndicator />
+    <PruviousToaster />
+    <component :is="dashboardHeadComponent()" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { dashboardHeadComponent, dashboardMiscComponent } from '#pruvious/dashboard'
+
+const PruviousLoadingIndicator = dashboardMiscComponent.LoadingIndicator()
+const PruviousToaster = dashboardMiscComponent.Toaster()
+</script>
